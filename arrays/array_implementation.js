7eddd4eef52f3d12e28e62d3a450c5d6589110c4
Anshuman_Bhowmick <anshumanbhowmick2@gmail.com>
2021-11-09 18:03:02 +0530

    first commit in dsinjs

---
+++ arrays/array_implementation.js
@@ -0,0 +1,10 @@
+// declaring an array
+var zero = 0;
+
+// creating a postive number array
+var postiveNumbers = [1, 2, 3];
+// creating a negative number array
+var negativeNumbers = [-1, -2, -3];
+// now concatinating the arrays
+var numbers = negativeNumbers.concat(zero, postiveNumbers);
+console.log(numbers);
