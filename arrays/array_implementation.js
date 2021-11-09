9ad39942e0fd7573499d1fe4be1316b7a9af5c46
Anshuman_Bhowmick <anshumanbhowmick2@gmail.com>
2021-11-09 18:45:57 +0530

    array methods(every,foreach,sort)

--- arrays/array_implementation.js
+++ arrays/array_implementation.js
@@ -8,3 +8,16 @@ var negativeNumbers = [-1, -2, -3];
 // now concatinating the arrays
 var numbers = negativeNumbers.concat(zero, postiveNumbers);
 console.log(numbers);
+
+var isEven = function (x) {
+  console.log(x);
+  return x % 2 == 0 ? true : false;
+};
+var numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers1.every(isEven)); //The Array.every() method returns true if all elements in an array pass a test (provided as a function).
+var numbers2 = [2, 1, 4, 3, 7, 5];
+console.log(numbers2.sort()); // sorting the array in ascending order
+numbers2.forEach(function (x) {
+  if (x % 2 == 0) console.log("even");
+  else console.log("odd");
+});
